a9f62807003c95917e9fa45674b0cba86c5e706c
Juan David Pareja Soto <parejajd@outlook.com>
2021-09-04 08:17:36 -0500

    Declaración y uso de variables

---
+++ 1.variables/variables.dart
@@ -0,0 +1,14 @@
+void main() {
+  var nombre = "Hola Mundo";
+  String nombre_dos = "desde Flutter";
+  var edad = 33;
+  int edad_dos = 33;
+  int? nullValue = null; //Null Safe Operator
+
+  //Variable de tipo Final
+  final nombre_final = "Hola Mundo";
+  //Variable de tipo Const
+  const PI = 3.1416;
+
+  print(nombre.toUpperCase() + " " + nombre_dos.toUpperCase());
+}
